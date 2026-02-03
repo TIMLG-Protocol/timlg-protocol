15753803c45e70334d2e5ccc3b084f1eb7bbe0e8
Richard David Martín Martín <elcorreoderichard@gmail.com>
2026-02-03 23:30:48 +0000

    feat: perfect horizontal timeline alignment with dynamic CSS bands and anchor milestones

---
+++ docs/assets/beta/assets/chunk-4BX2VUAB-CP0v9wr7.js
@@ -0,0 +1,16 @@
+import { _ as __name } from "./index-B0nY6KpU.js";
+function populateCommonDb(ast, db) {
+  if (ast.accDescr) {
+    db.setAccDescription?.(ast.accDescr);
+  }
+  if (ast.accTitle) {
+    db.setAccTitle?.(ast.accTitle);
+  }
+  if (ast.title) {
+    db.setDiagramTitle?.(ast.title);
+  }
+}
+__name(populateCommonDb, "populateCommonDb");
+export {
+  populateCommonDb as p
+};
